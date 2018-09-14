9b1ded1a1b951f9e9548fb7ff9019e9956f6fbfe
Eliot Fondere <fondere.eliot@cra.education>
2018-09-14 16:54:39 -0400

    finish Exercice 2-1

--- Exercice 2-1/main.cpp
+++ Exercice 2-1/main.cpp
@@ -1,3 +1,4 @@
+#include <exception>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -5,19 +6,49 @@
 char        oper, continueConfirm;
 int         num1, num2;
 
+int processMath()
+{
+    switch (oper)
+    {
+    case '+':
+        return num1 + num2;
+        break;
+    case '-':
+        return num1 - num2;
+        break;
+    case '*':
+        return num1 * num2;
+        break;
+    case '/':
+        if (!num2 == 0)
+            return num1 / num2;
+        break;
+    default:
+        throw std::runtime_error("Invalid operator");
+        break;
+    }
+}
+
 int main(int argc, char *argv[], char *envp[])
 {
     std::cout << "Exercice 2-1 - Calculette de console" << std::endl;
 
     do
     {
-        std::cout << "Entrez le premier nombre : " << std::flush;
-        std::cin >> num1;
-        std::cout << "Entrez le premier nombre : " << std::flush;
-        std::cin >> num1;
-    } while (continueConfirm != 'y');
+        try {
+            std::cout << "\nEntrez la formule : " << std::flush;
+            std::cin >> num1 >> oper >> num2;
+            std::cout << "\nResultat : " << processMath() << std::endl;
 
-    std::cin >> userInput;
+            std::cout << "Voulez-vous continuer (o/n)? " << std::endl;
+            std::cin >> continueConfirm;
+        }
+        catch (std::exception e)
+        {
+            std::cout << "Donnee invalides, veuillez re-entrer les informations" << std::endl;
+            continueConfirm = 'o';
+        }
+    } while (continueConfirm == 'o');
 
     return 0;
 }
